0e31d3b5da28d7c6c229910e54fc19ae3c46ce0b
LRhytz <rhytz.133@gmail.com>
2024-03-16 17:25:06 +0800

    Archival First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Pokedex from './Pokedex'
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <div className="App">
+      <Pokedex />
+    </div>
+    </>
+  );
+}
+
+export default App;
